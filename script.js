80a1d912d47f858107f2e8c1d34b69868f60cfd1
mhtzinxs <160683080+mhtzinxs@users.noreply.github.com>
2025-05-16 22:10:05 -0300

    arquivos essenciais

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarMensagem() {
+    document.getElementById('mensagem').textContent = "Cada passo que dei me trouxe até este instante,onde o passado se despede e o futuro é um mistério vibrante.O que virá a seguir, só o tempo irá revelar,mas carrego em meu coração a coragem de sonhar.--mhtzinxs";
+}
